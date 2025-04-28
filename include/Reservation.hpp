3a0df13a6afb2cf1ad3f59aaafcf302dbbea9073
Alireza <alirezasharifi998@gmail.com>
2025-04-28 14:59:34 +0330

    Add getters and setters to reservation

--- include/Reservation.hpp
+++ include/Reservation.hpp
@@ -19,6 +19,21 @@ public:
     Reservation();
     Reservation(int reservationID, Student &student, const DiningHall &dHall, const Meal &meal, RStatus status, time_t createdAT);
 
+    // Getters
+    int getReservationID() const { return _reservationID; }
+    Student &getStudent() const { return _student; }
+    const DiningHall &getDiningHall() const { return _dHall; }
+    const Meal &getMeal() const { return _meal; }
+    RStatus getStatus() const { return _status; }
+    time_t getCreatedAT() const { return _createdAT; }
+
+    // Setters
+    void setReservationID(int reservationID) { _reservationID = reservationID; }
+    void setDiningHall(const DiningHall &dHall) { _dHall = dHall; }
+    void setMeal(const Meal &meal) { _meal = meal; }
+    void setStatus(RStatus status) { _status = status; }
+    void setCreatedAT(time_t createdAT) { _createdAT = createdAT; }
+
 private:
     int _reservationID;
     Student &_student;
